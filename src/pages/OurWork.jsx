5b49aed72c5c748b8aa7e452b6fcd5f3d54739c6
Fidel Collin <fidelcollin@icloud.com>
2021-02-24 19:47:16 +0000

    added styling and content to OurWork page

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -1,9 +1,64 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
 
 const OurWork = () => {
-  return <div>
-    <h2>Our Work</h2>
-  </div>;
+  return (
+    <Work>
+      <Movie>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="athlete" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="the racer" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2> Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="goodtimes" />
+        </Link>
+      </Movie>
+    </Work>
+  );
 };
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  width: 90vw;
+  margin: auto;
+  padding: 5rem 0;
+
+  h2 {
+    padding: 1rem 0;
+  }
+`;
+
+const Movie = styled.div`
+  padding-bottom: 10rem;
+
+  .line {
+    height: 0.5rem;
+    /* width: 100%; */
+    background: #fff;
+    margin-bottom: 3rem;
+  }
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+`;
 
 export default OurWork;
