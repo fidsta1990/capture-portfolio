6acdc29f3bdd2e2a14fb1e24d3df5814e71bd57c
Fidel Collin <fidelcollin@icloud.com>
2021-03-01 16:23:33 +0000

    add animation for active page on navbar links. Created responsive positioning on mobile screen for line

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
   return (
     <Nav>
       <div className="nav-wrap">
@@ -16,16 +19,31 @@ const Navbar = () => {
             <Link to="/" href="#">
               1. About Us
             </Link>
+            <Line
+              transition={{ duration: 0.5 }}
+              initial={{ width: 0 }}
+              animate={{ width: pathname === `/` ? "50%" : "0%" }}
+            />
           </li>
           <li>
             <Link to="/work" href="#">
               2. Our Work
             </Link>
+            <Line
+              transition={{ duration: 0.5 }}
+              initial={{ width: 0 }}
+              animate={{ width: pathname === `/work` ? "50%" : "0%" }}
+            />
           </li>
           <li>
             <Link to="/contact" href="#">
               3. Contact Us
             </Link>
+            <Line
+              transition={{ duration: 0.5 }}
+              initial={{ width: 0 }}
+              animate={{ width: pathname === `/contact` ? "50%" : "0%" }}
+            />
           </li>
         </ul>
       </div>
@@ -81,8 +99,21 @@ const Nav = styled.nav`
     position: relative;
 
     @media (max-width: 1300px) {
-     padding-left:0;
+      padding-left: 0;
     }
+  }
+`;
+
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  position: absolute;
+  width: 0%;
+  bottom: -80%;
+  left: 60%;
+
+  @media (max-width: 1300px) {
+    left: 0;
   }
 `;
 
